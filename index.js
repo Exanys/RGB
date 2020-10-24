7135e36c206185f7e6658b5c39890c67d35db5d1
Exanys <exanys226@gmail.com>
2020-10-24 11:28:20 +0200

    s chybami02

--- index.js
+++ index.js
@@ -10,23 +10,30 @@ let chybne = 0;
 
 
 
-btn.addEventListener("click",vypis());
-
-function prevod(value){
-        if(value > 255 || value < 0){
-            chybne = parseInt(value);
-        }else
-           RGBhex.push(parseInt(value).toString(16));   
-}   
-
-function vypis(){
+btn.addEventListener("click",function vypis(){
     prevod(RGB[0]);
     prevod(RGB[1]);
     prevod(RGB[2]);
     if(chybne > 0){
-        vystup.innerHTML = `<p>${chybne} neni v rozsahu</p>`;
+        vystup.innerHTML = `<p>RGB barva hexadecimalne: ${chybne} neni v rozsahu</p>`;
     }
     else{
-        vystup.innerHTML = `<p>#${RGBhex[0]}${RGBhex[1]}${RGBhex[2]}`;
+        vystup.innerHTML = `<p>RGB barva hexadecimalne: #${RGBhex[0]}${RGBhex[1]}${RGBhex[2]}</p>`;
     }
+    clean();
+})
+
+function prevod(value){
+        if(value > 255 || value < 0){
+            chybne = parseInt(value);
+        }else if(value >= 0 && value < 10){
+            RGBhex.push(`0${value.toString()}`);
+        }else
+           RGBhex.push(parseInt(value).toString(16));   
+}   
+function clean(){
+    RGB = [];
+    R.value = 0;
+    R.value = 0;
+    R.value = 0;
 }
