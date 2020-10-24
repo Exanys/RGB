2993ac05beae268a1fd2575593e77ae14139ae52
Exanys <exanys226@gmail.com>
2020-10-24 10:59:14 +0200

    s chybami

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const B = document.getElementById("B");
 const vystup = document.getElementById("vystup");
 let RGB = [R.value, G.value, B.value];
 let RGBhex = [];
-let chybne;
+let chybne = 0;
 
 
 
@@ -20,5 +20,13 @@ function prevod(value){
 }   
 
 function vypis(){
-    
+    prevod(RGB[0]);
+    prevod(RGB[1]);
+    prevod(RGB[2]);
+    if(chybne > 0){
+        vystup.innerHTML = `<p>${chybne} neni v rozsahu</p>`;
+    }
+    else{
+        vystup.innerHTML = `<p>#${RGBhex[0]}${RGBhex[1]}${RGBhex[2]}`;
+    }
 }
