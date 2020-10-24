daeb91edad33f9ab7592b9edc32c69cded7c8565
Exanys <exanys226@gmail.com>
2020-10-24 10:50:50 +0200

    oprava

--- index.js
+++ index.js
@@ -2,22 +2,23 @@ const btn =document.getElementById("btn");
 const R = document.getElementById("R");
 const G = document.getElementById("G");
 const B = document.getElementById("B");
+const vystup = document.getElementById("vystup");
 let RGB = [R.value, G.value, B.value];
 let RGBhex = [];
-let nmb = 255;
-let hex = nmb.toString(16);
+let chybne;
 
 
 
-btn.addEventListener("click",prevod());
 
-function prevod(){
-    RGB.forEach(function(value){
+btn.addEventListener("click",vypis());
+
+function prevod(value){
         if(value > 255 || value < 0){
-            console.log(`${value} neni v platnem rozsahu`);
-        }else{
-           RGBhex.push(parseInt(value).toString(16));
-        }
-    })
-    console.log(RGBhex);    
+            chybne = parseInt(value);
+        }else
+           RGBhex.push(parseInt(value).toString(16));   
 }   
+
+function vypis(){
+    
+}
